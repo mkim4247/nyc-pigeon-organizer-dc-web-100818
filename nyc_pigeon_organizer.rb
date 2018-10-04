e628e56c1aab8df0ad43e0452fca11c9bae7d340
Michael Kim <m.kim4247@gmail.com>
2018-10-04 22:50:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {} 
+  
+  data.each do |main_stat, stat_name|
+    stat_name.each do |attribute, array|
+      array.each do |name|
+        pigeon_hash[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+
+  data[:color].each do |color_name, pig_array|
+    pig_array.each do |name|
+      pigeon_hash[name][:color] << color_name.to_s
+    end 
+  end 
+  
+  data[:gender].each do |sex, pig_array|
+    pig_array.each do |name|
+      pigeon_hash[name][:gender] << sex.to_s
+    end 
+  end 
+  
+  data[:lives].each do |loc, pig_array|
+    pig_array.each do |name|
+      pigeon_hash[name][:lives] << loc.to_s 
+    end 
+  end 
+  
+  pigeon_hash
+
 end
+
