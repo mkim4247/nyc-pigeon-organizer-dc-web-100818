0b560fbd1b07ac15e95bbc72bfb074566149383e
Michael Kim <m.kim4247@gmail.com>
2020-10-18 14:47:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,33 +1,23 @@
-def nyc_pigeon_organizer(data)
-  pigeon_hash = {} 
-  
-  data.each do |main_stat, stat_name|
-    stat_name.each do |attribute, array|
-      array.each do |name|
-        pigeon_hash[name] = {:color => [], :gender => [], :lives => []}
-      end 
-    end 
-  end 
-
-  data[:color].each do |color_name, pig_array|
-    pig_array.each do |name|
-      pigeon_hash[name][:color] << color_name.to_s
-    end 
-  end 
-  
-  data[:gender].each do |sex, pig_array|
-    pig_array.each do |name|
-      pigeon_hash[name][:gender] << sex.to_s
-    end 
-  end 
-  
-  data[:lives].each do |loc, pig_array|
-    pig_array.each do |name|
-      pigeon_hash[name][:lives] << loc.to_s 
-    end 
-  end 
-  
-  pigeon_hash
-
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  #iterate down to each name and add them to new hash
+  pigeon_data.each do |attribute, description_hash|
+    #:color, :purple => {"Theo", ...}
+    description_hash.each do |description, names_arr|
+      #:purple => ['Theo', ...]
+      names_arr.each do |name|
+        if pigeon_list[name].nil?
+        # if name key doesn't already exist in pigeon_list, create it
+          pigeon_list[name] = {}
+          # pigeon_list = { "Theo" => {}, "Peter" => {},... }
+        end
+        if names_arr.include?(name)
+        # if name exists in the name array, add an empty array
+          pigeon_list[name][attribute] = []
+          pigeon_list[name][attribute] << description.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
-
